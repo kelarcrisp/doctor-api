9fbfdb0fc3dd7110374c7c5b70ff5cf59ab0d049
Student <example@example.com>
2019-07-05 15:45:19 -0700

    everything works except for the website

--- src/main.js
+++ src/main.js
@@ -44,17 +44,22 @@ $(".search").submit(function(event){
       $(".output1").text(``);
     }
 
+
     for (let i = 0; i < result.data.length; i++) {
 
     $('.output1').append(`<strong>located in: ${result.data[i].practices[0].location_slug} <br>`);
     $('.output1').append(`<strong>Phone number: ${result.data[i].practices[0].phones[i].number} <br>`);
-    $('.output1').append(`bio: ${result.data[i].profile.bio} <br>`);
+    $('.output1').append(`<i>bio: ${result.data[i].profile.bio} <br>`);
       $('.output1').append(`<strong>Accepting new patients: ${result.data[i].practices[0].accepts_new_patients} <br>`);
       $('.output1').append(`<strong>First Name:</bold> ${result.data[i].profile.first_name}<br> `);
       $('.output1').append(`<strong>Last Name ${result.data[i].profile.last_name} <hr>`);
         $('.output1').append(`<strong>street address: ${result.data[i].practices[0].visit_address.street}<br> `);
-        // $('.output1').append(`website:  ${result.data[i].practices[0].website}<br>`);
-        $(".output1").append("<img src='" + result.data[i].profile.image_url+ "'>")
+        $('.output1').append(`website:  ${result.data[i].practices[0].website}<br>`);
+        $(".output1").append("<img src='" + result.data[i].profile.image_url+ "'>");
+
+        if (result.data[i].practices[0] === "" ){
+        $(".output1").append('this user doesnt have a website');
+        }
 
     }
   },function(error) {
